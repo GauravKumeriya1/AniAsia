899282f33d60c036e76c87ea051bf1c18cad18a7
GauravKumeriya1 <gauravkumeriya25@gmail.com>
2023-02-04 16:08:50 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ _config.php
@@ -0,0 +1,29 @@
+<?php 
+
+$websiteTitle = "AniKatsu"; // Website Name
+$websiteUrl = "//{$_SERVER['SERVER_NAME']}";  // Website URL
+$websiteLogo = $websiteUrl . "/files/images/logo.png?v=2"; // Logo
+$contactEmail = "xyz@gmail.com"; // Contact Email
+
+$version = "0.3";
+
+//Donate 
+$donate = "#";
+
+// Socials 
+$telegram = "https://t.me/#"; // telegram
+$discord = "https://discord.com/#"; // Discord
+$redit = "#"; // Reddit
+$twitter = "#"; // Twitter
+ 
+
+
+$disqus = "https://indianime.disqus.com"; // Disqus
+
+
+// API URL
+$api = "https://wordywelcomepostscript.shashankktiwari.repl.co"; // https://github.com/shashankktiwariii/anikatsu-api
+
+
+$banner = $websiteUrl . "/files/images/banner.png";  //Banner
+?>
